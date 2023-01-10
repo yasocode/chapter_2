3fae7c10f6f550720ab57e4b198e83ffcb2b3826
Yasin Konyalı <yasinkonyali12@gmail.com>
2023-01-10 16:08:39 +0300

    chapter 2

---
+++ sandbox.js
@@ -0,0 +1,74 @@
+//template strings
+
+// const title = 'Best cars of 2019'
+// const author = 'BMW'
+// const likes = 30;
+
+// let result = 'This blog called ' + title + ' by ' + author + ' has ' + likes + ' likes.';
+
+// let result = `This blog called ${title} by ${author} has ${likes} likes.`;
+
+// let html = `
+//     <h2>${title}</h2>
+//     <p>By ${author}</p>
+//     <span>This blog has ${likes} likes.</span>
+// `
+// console.log(html);
+
+// let ninjas = ['ahmet', 'mehmet', 'hasan'];
+
+// let result = ninjas.join(';');
+// let result= ninjas.concat(['ayşe', 'fatma']);
+// let result = ninjas.push('hatice');
+// result = ninjas.pop();
+
+// console.log(ninjas);
+
+// console.log(true, false, "true", "false");
+
+// let email = 'someone@somewhere.com';
+// let names = ['mirana', 'pudge']
+
+// let result = email.includes('@');
+// let result = name.includes('pudge');
+
+// let age = 25;
+
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age != 30);
+// console.log(age > 25);
+// console.log(age < 25);
+// console.log(age <= 25);
+
+// let name = 'hasan';
+
+// console.log(name == 'hasan');
+// console.log(name == 'Hasan');
+// console.log(name > 'mehmet');
+// console.log(name > 'Hasan');
+// console.log(name > 'Can');
+
+// let age = 25;
+
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log(age != 25);
+// console.log(age != '25');
+
+
+// console.log(age === 25);
+// console.log(age === '25');
+// console.log(age !== 25);
+// console.log(age !== '25');
+
+
+// let score = '100';
+
+// // score = Number(score);
+// console.log(score + 1);
+// console.log(typeof score);
+
+// let result = String(50);
+
+// console.log(result, typeof result);
